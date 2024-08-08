c0d7836cacfd3eacce644a6d67207e5428c9c509
Teme Azanaw <tazanaw0@frostburg.edu>
2024-08-08 14:18:46 -0400

    mapped album (3) views to varying urls

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views #from this folder (base) import all the views
 
 urlpatterns =[
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('candidPhotography/', views.candid, name='candid'),
+    path('landscapePhotography/', views.landscape, name='landscape'),
+    path('streetPhotography/', views.street, name='street')
     #path('socials/', views.socials, name='socials'),
 ]
  
