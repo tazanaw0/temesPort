5ecfe2794a4a5628d0a0876276325dfb74a96c9c
Teme Azanaw <tazanaw0@frostburg.edu>
2024-08-09 13:20:55 -0400

    fetch images for our Photo model using objects.filter() method

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Photo
 # Create your views here. 
 # created a function name home that will render my home.html file when app is requested.
 def home (request):
@@ -10,7 +11,8 @@ def home (request):
     return render(request, 'home.html', context)
 #Album of candid photos 
 def candid(request):
-    return render(request, 'candidPhotography.html')
+    photos = Photo.objects.filter(category='candid') #variable defined to fetch and filter (object.filter() method) photos based on their category 
+    return render(request, 'candidPhotography.html', {'photos': photos})
 
 #Album of landscape photos
 def landscape(request):
