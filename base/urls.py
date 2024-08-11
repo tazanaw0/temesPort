1ea0de81a0c9f0c631789f4b5a696e479a430981
Teme Azanaw <tazanaw0@frostburg.edu>
2024-08-11 17:30:21 -0400

    renamed candid ->portrait

--- base/urls.py
+++ base/urls.py
@@ -3,9 +3,8 @@ from . import views #from this folder (base) import all the views
 
 urlpatterns =[
     path('', views.home, name='home'),
-    path('candidPhotography/', views.candid, name='candid'),
+    path('Portraits/', views.portraits, name='portraits'),
     path('landscapePhotography/', views.landscape, name='landscape'),
     path('streetPhotography/', views.street, name='street'),
     #path('socials/', views.socials, name='socials'),
 ] 
- 
