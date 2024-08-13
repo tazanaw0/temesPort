33e18968f6bcec9d12190ddfe99bf9ebc2e7852a
Teme Azanaw <tazanaw0@frostburg.edu>
2024-08-13 18:42:12 -0400

    added iphone photography path

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views #from this folder (base) import all the views
 
 urlpatterns =[
     path('', views.home, name='home'),
+    path('iPhone/', views.iPhone, name='iPhone'),
     path('Portraits/', views.portraits, name='portraits'),
     path('landscapePhotography/', views.landscape, name='landscape'),
     path('streetPhotography/', views.street, name='street'),
