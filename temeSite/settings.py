a2de3b06aa050a0c0f69138e7406d9a180909dc3
Teme Azanaw <tazanaw0@frostburg.edu>
2024-08-28 19:51:33 -0400

    added media context processors to TEMPLATES

--- temeSite/settings.py
+++ temeSite/settings.py
@@ -63,8 +63,10 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
